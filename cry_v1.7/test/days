a0dc4971acf10f2da6dfc9e5abc31002b8d496cd
cimorales2 <cimorales2@uc.cl>
2016-11-30 12:44:41 -0300

    New data

--- cry_v1.7/test/days
+++ cry_v1.7/test/days
@@ -1,13 +1,14 @@
 import numpy as np
 import os
 
-path = '/home/cristobalmr/Desktop/Invest/cry_v1.7/test/'
+path = os.getcwd()
+print path
 
 n=0
 p=0
 g=0
 lat=90
-alt=0
+alt=11300
 sbboxlength = 100
 sim=1000
 
@@ -15,11 +16,11 @@ sim=1000
 for i in range(2012,2016):#y
     for j in range(1,13):#m
         for z in range(1,29):#d
-            f = open(path+'setup.file','w')
+            f = open(path+'/setup.file','w')
             f.write(
-            'returnNeutrons {0}\nreturnProtons {1}\nreturnGammas {2}\n date {3}-{4}-{5}\n latitude {6}\n altitude {7}\nsubboxLength {8}'.format(n,p,g,j,z,i,lat,alt,sbboxlength))
+            'returnNeutrons {0}\nreturnProtons {1}\nreturnGammas {2}\ndate {3}-{4}-{5}\nlatitude {6}\naltitude {7}\nsubboxLength {8}'.format(n,p,g,j,z,i,lat,alt,sbboxlength))
             f.close()
-            os.system('./testMain setup.file {3} | grep muon  > ~/Desktop/Invest/outputdate/date{0}-{1}-{2}.dat'.format(j,z,i,sim))
+            os.system('./testMain setup.file {3} | grep muon  > /home/cristobalmr/Desktop/Investigation/outputdate/date{0}-{1}-{2}.dat'.format(j,z,i,sim))
             print z,j,i
 
 print 'Done!'
--- save_data.py
+++ save_data.py
@@ -1,23 +1,25 @@
 import numpy as np
+import matplotlib.pyplot as plt
 import os
 
-X,Y,Z,KE=np.empty((0,1),object),np.empty((0,1),object),np.empty((0,1),object),np.empty((0,1),object)
+e=np.array([])
 a = len(os.listdir(os.getcwd()))
-for i in os.listdir(os.getcwd()):
+for filename in os.listdir(os.getcwd()):
     print a
     a -=1
-    if i == 'get_Data': continue
-    f = open(i)
-    f = f.read()
-    f = f.strip('\n').split('\n')
-    f = f[6:]
-    for j in f:
-        j = j.split()
-        x,y,z,ke = float(j[5]),float(j[6]),float(j[7]),float(j[3].split('=')[1])
-        X = np.append(X,np.array([[x]]),axis=0)
-        Y =np.append(Y,np.array([[y]]),axis=0)
-        Z = np.append(Z,np.array([[z]]),axis=0)
-        KE = np.append(KE,np.array([[ke]]),axis=0)
+    if filename == 'get_Data': continue
+    try:  e = np.append(e, np.loadtxt(filename, dtype='str',usecols=(3,)))
+    except: pass
 
-unified = np.hstack((X,Y,Z,KE))
-np.savetxt('pos_ke.dat',unified)
+e = np.array([float(z.split('=')[1]) for z in e])
+
+n,bins,patches=plt.hist(e,bins=np.arange(min(e),max(e),100),color='k')
+plt.xlim(0,30000)
+plt.xlabel('ke')
+plt.show()
+print n.max()
+
+
+#max 90deg ~ 38750
+#max 0deg ~ 41664
+#max -33def ~ 41664
